1edc506f1e7465bd70a60e8492dafc6d1d71e536
Jose Luis Checa Exposito <joselchecaexpo@gmail.com>
2018-10-24 17:04:30 +0100

    serve static main.js

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express')
+const path = require('path')
+
 const routes = require('./src/server/routes/routes')
 
 const app = express()
 
 const port = process.env.PORT || 3000
+
+// Serving static files
+app.use(express.static(path.join(__dirname, './dist')));
 
 routes(app)
 
--- src/server/views/view.js
+++ src/server/views/view.js
@@ -5,6 +5,7 @@ module.exports = ({ content = 'Its Running', script = '', store = '', title = ''
         <title>${title}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="apple-mobile-web-app-capable" content="yes">
+        <script src="main.js"></script>
       </head>
       <body>
         <div id="root">${content}</div>
