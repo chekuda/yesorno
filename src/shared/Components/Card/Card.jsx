b3abf3affdef68b369f0dd8bfe50730a9d0daae2
Jose Luis Checa Exposito <joselchecaexpo@gmail.com>
2018-11-03 11:02:54 +0000

    customize votes ui

--- src/shared/Components/Card/Card.jsx
+++ src/shared/Components/Card/Card.jsx
@@ -4,23 +4,24 @@ if(process.browser) {
   require('./Card.scss')
 }
 
-const Card = ({ content, handleOnClick, id }) => {
+const Card = ({ content, yes, no, handleOnClick, id }) => {
   return (
     <div className='card-container'>
       <div className='content'>
         { content }
       </div>
       {
-        handleOnClick && 
-          <div className='action yes' onClick={() => handleOnClick(id, 'yes')}>
-            <img src='/assets/icons/thumbs-up.svg' width='25' height="25" />
-          </div>
-      }
-      {
-        handleOnClick && 
-          <div className='action no' onClick={() => handleOnClick(id, 'no')}>
-            <img src='/assets/icons/thumbs-down.svg' width='25' height="25" />
-          </div>
+        handleOnClick &&
+          <div className='card-section'>
+            <div className='action' onClick={() => handleOnClick(id, 'yes')}>
+              <img src='/assets/icons/smile.svg' width='25' height="25" />
+              { yes && <p className='votes'>{yes}</p> }
+            </div>
+            <div className='action' onClick={() => handleOnClick(id, 'no')}>
+              { no && <p className='votes'>{no}</p> }
+              <img src='/assets/icons/frown-open.svg' width='25' height="25" />
+            </div>
+        </div>
       }
     </div>
   )
